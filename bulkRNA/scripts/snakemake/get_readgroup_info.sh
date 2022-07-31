a8913a5587e115fc4949d03089c503377eea9fb3
olneykimberly <olneykimberly@gmail.com>
2022-07-31 10:56:24 -0500

    add scripts

--- bulkRNA/scripts/snakemake/get_readgroup_info.sh
+++ bulkRNA/scripts/snakemake/get_readgroup_info.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # change directory
-cd /research/labs/neurology/fryer/projects/sepsis/pig/LPS/bulkRNA
+cd ../../rawfastq
 
 # create file with list of R1 samples
 ls -1 | grep _R1_ > R1Samples.txt
 
 # change directory 
 
-# loops through list and print first line
+# loops through list 
 touch sampleReadInfo.txt
 for sample in `cat R1Samples.txt`; do
-    #printf "${sample}\t"
     zcat ${sample} | head -1 >> sampleReadInfo.txt	
 done;
 
-mv R1Samples.txt  /research/labs/neurology/fryer/m239830/SEPSIS/code/git/LPS_Pigs/bulkRNA/scripts/snakemake/R1Samples.txt
+# mv the files 
+mv R1Samples.txt  /LPS_pigs/bulkRNA/scripts/snakemake/R1Samples.txt
+mv sampleReadInfo.txt /LPS_pigs/bulkRNA/scripts/snakemake/sampleReadInfo.txt
 
-mv sampleReadInfo.txt /research/labs/neurology/fryer/m239830/SEPSIS/code/git/LPS_Pigs/bulkRNA/scripts/snakemake/sampleReadInfo.txt
-
-cd /research/labs/neurology/fryer/m239830/SEPSIS/code/git/LPS_Pigs/bulkRNA/scripts/snakemake//
+cd /LPS_pigs/bulkRNA/scripts/snakemake/
 paste -d "\t" R1Samples.txt sampleReadInfo.txt > sampleReadGroupInfo.txt
 rm R1Samples.txt
 rm sampleReadInfo.txt
+
